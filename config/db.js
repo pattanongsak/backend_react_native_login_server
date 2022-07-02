5ecee0ad15cdaae3bb1191098fab658b924421b7
unknown <you@example.com>
2022-07-02 22:12:14 +0700

    letffo

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
-
+// console.log(process.env.MONGODB_URI)
 mongoose
     .connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
+        // useUnifiedTopology: true,
     })
     .then(() => {
         console.log("DB Connected")
