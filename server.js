97357c04ad395e7efbf2f5dc3571fe3f62e0e990
unknown <you@example.com>
2022-07-02 22:41:50 +0700

    sssd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('./config/db');
 
 const app = require('express')();
-const port =  process.env.PORT || 3000;
+const port = process.env.PORT || 9001;
 
 const UserRouter = require('./api/User');
 
@@ -10,6 +10,9 @@ const UserRouter = require('./api/User');
 const bodyParser = require('express').json;
 app.use(bodyParser());
 
+app.get('/home', (req, res) => {
+    res.status(200).send('Hellow Wolrd')
+})
 app.use('/user', UserRouter)
 
 app.listen(port, () => {
